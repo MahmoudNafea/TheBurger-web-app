a9b8dbed83d6eb3bbf73bc519c18bd65e6f2ca7c
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-08-17 16:53:09 +0800

    using redux with asynch code

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import ContactData from './ContactData/ContactData';
@@ -11,16 +11,21 @@ class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data');
     }
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    CheckoutCancelled={this.CheckoutCancelledHandler}
-                    CheckoutContinued={this.CheckoutContinuedHandler} />
-                <Route path={this.props.match.path + '/contact-data'}
-                    component={ContactData} />
-            </div>
-        )
+        let summary = <Redirect to='/' />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        CheckoutCancelled={this.CheckoutCancelledHandler}
+                        CheckoutContinued={this.CheckoutContinuedHandler} />
+                    <Route path={this.props.match.path + '/contact-data'}
+                        component={ContactData} />
+                </div>
+            )
+        }
+
+        return summary;
     }
 };
 
---
+++ src/store/actions/index.js
@@ -0,0 +1,4 @@
+// setting all the imports here as a central file
+export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder';
+export { purchaseBurger, purchaseBurgerStart } from './order';
+
